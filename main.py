af5a2f001ae024b6410ec122895a33b2cd6c4f7f
bhavesha <bhavesha.talreja@gmail.com>
2026-02-15 01:53:49 +0530

    Emailer to send news

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 import requests
 
+import send_email
+
 API_KEY = "9b820b3d72d74cfe907017d6b572d6d2"
-url = f"https://newsapi.org/v2/everything?q=tesla&sortBy=publishedAt&apiKey={API_KEY}"
+url = (f"https://newsapi.org/v2/everything?"
+       f"q=tesla&sortBy=publishedAt&apiKey={API_KEY}&language=en")
 
 request = requests.get(url)
 content = request.json()
 
-for item in content["articles"]:
-    print(item["title"])
-    print(item["description"])
+lines = []
+
+for item in content["articles"][:20]:
+    title = item.get("title") or ""
+    if title is not None:
+        description = item.get("description") or ""
+        url = item.get("url")
+        lines.append(f"{title}\n{description}\n{url}")
+
+emailbody = "Subject: Today's News\n\n".join(lines)
+print("emailbody",emailbody)
+
+emailbody = emailbody.encode('utf-8')
+send_email.send_email(emailbody)
